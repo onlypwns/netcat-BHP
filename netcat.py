8bb59f2f27ac2184c9e2b58deaa1d89e0d9a21b9
bashingrats <88723154+thr10en4@users.noreply.github.com>
2022-03-25 16:19:25 -0400

    Update netcat.py

--- netcat.py
+++ netcat.py
@@ -6,49 +6,23 @@ import sys
 import textwrap
 import threading
 
-def exexute(cmd):
+
+def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
-    output = subprocess.check_output(shlex.split(cmd), sderr=subprocess.STOUT)
-
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return output.decode()
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='BHP Net Tool', formatter_class=argparse.RawDescriptionHelpFormatter, 
-    epilog=textwrap.dedent('''Example:
-    netcat.py -t 192.168.1.108 -p 5555 -l -c
-    netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt 
-    netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\"
-    echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 
-    netcat.py -t 192.168.1.108 -p 5555 
-    '''))
-
-parser.add_argument('-c', '--command', action='store_true', help='command shell')
-parser.add_argument('-e', '--execute', help='execute specified command')
-parser.add_argument('-l', '--listen', action='store_true', help='listen')
-parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
-parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
-parser.add_argument('-u', '--upload', help='upload file')
-
-args = parser.parse_args()
-if args.listen:
-    buffer = ''
-else:
-    buffer = sys.stdin.read()
-
-nc = Netcat(args, buffer.encode())
-nc.run()
 
 class Netcat:
     def __init__(self, args,buffer=None):
         self.args = args
         self.buffer = buffer
-        self.socket = socket.socket(socket, AF_INET, socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     def run(self):
         if self.args.listen:
-            self.isten()
+            self.listen()
         else:
             self.send()
     def send(self):
@@ -119,3 +93,45 @@ class Netcat:
                     print(f'Server killed {e}')
                     self.socket.close()
                     sys.exit()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='BHP Net Tool', formatter_class=argparse.RawDescriptionHelpFormatter, 
+    epilog=textwrap.dedent('''Example:
+    netcat.py -t 192.168.1.108 -p 5555 -l -c
+    netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt 
+    netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\"
+    echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 
+    netcat.py -t 192.168.1.108 -p 5555 
+    '''))
+
+    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+    parser.add_argument('-e', '--execute', help='execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
+    parser.add_argument('-u', '--upload', help='upload file')
+
+    args = parser.parse_args()
+    
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+
+    nc = Netcat(args , buffer.encode())
+    nc.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
